d4f4ab550a2acf43bf14c6c59501239569fba22f
ADRIAN RINCON <107867643+AdrianYRincon@users.noreply.github.com>
2023-03-17 12:00:07 -0500

    Update emailRegistro.js

--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -15,7 +15,7 @@ const emailRegistro = async (datos) => {
 
   //Enviar el email
   const info = await transporter.sendMail({
-    from: "APV - Administrador Pacientes Veterinaria" <apv@correo.com>",
+    from: '"APV - Administrador Pacientes Veterinaria" <apv@correo.com>',
     to:email,
     subject: 'Comprueba tu cuenta en APV',
     text: 'Comprueba tu cuenta en APV',
